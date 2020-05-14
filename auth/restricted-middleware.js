39af9cc19fbd4f0aa5d4e436649a28ff3b67b89c
Ali Rajaii <rajaii_a@yahoo.com>
2020-05-14 12:51:09 -0400

    restricted middleware wired in

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -6,17 +6,24 @@ const Users = require('../users/users-model.js');
 
 module.exports = (req, res, next) => {
 
-  const token = req.headers.authorization.split(' ')[1];
+  try {
+  const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
 
   if (token) {
     jwt.verify(token, secrets.jwt_secret, (err, decodedToken) => {
       if (err) {
+        throw new Error(err)
         res.status(401).json({message: 'bad auth'})
       } else {
         req.decodedToken = decodedToken
         next()
       }
     })
+  } else {
+    throw new Error()
+    res.status(401).json({message: 'bad auth'})
   }
-
+} catch (err) {
+  res.status(401).json({message: 'bad auth'})
+}
 };
